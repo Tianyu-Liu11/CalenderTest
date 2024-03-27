40ca38e87a8ed0c06e10a52ca6f57d0e97d44197
Tianyu_Liu <tianyu.liu@ninjatech.ai>
2024-03-27 22:59:41 +1100

    test

--- src/generated_code.py
+++ src/generated_code.py
@@ -1,2 +1,4 @@
 def query(calendar_data):
-    return len(calendar_data)
+    today_date = "2024-03-12"
+    today_events = calendar_data[(calendar_data['start'].str.contains(today_date)) & (calendar_data['status'] == 'confirmed')].count()
+    return today_events['ID']
