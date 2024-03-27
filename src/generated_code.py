ec224d8410b563ddbfeef28ed7878d302d87e0d7
Tianyu_Liu <tianyu.liu@ninjatech.ai>
2024-03-27 23:29:29 +1100

    Refresh teh cache and several key points

--- src/generated_code.py
+++ src/generated_code.py
@@ -1,4 +1,3 @@
 def query(calendar_data):
-    today_date = "2024-03-12"
-    today_events = calendar_data[(calendar_data['start'].str.contains(today_date)) & (calendar_data['status'] == 'confirmed')].count()
-    return today_events['ID']
+    scheduled_meetings = calendar_data[calendar_data['status'] != 'cancelled']
+    return len(scheduled_meetings)
