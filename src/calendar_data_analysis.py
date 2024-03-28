8cd57e9567fb720b25b2c23a834993a15702b16f
Tianyu_Liu <tianyu.liu@ninjatech.ai>
2024-03-28 16:19:15 +1100

    simplfied the code

--- src/calendar_data_analysis.py
+++ src/calendar_data_analysis.py
@@ -1,7 +1,5 @@
 """
     Description:
-
-
 """
 
 import os
@@ -12,13 +10,10 @@ import argparse
 from openai import OpenAI
 from dotenv import load_dotenv
 
-# refresh and reload the fucntion
-import importlib
-import GPT_4_generated_code
-
 
 def get_calendar_data(DATA_PATH):
     return pd.read_csv(DATA_PATH + "calendar_data.csv")
+
 
 def get_question(DATA_PATH):
     with open(DATA_PATH + "question.json") as json_file:
@@ -26,13 +21,15 @@ def get_question(DATA_PATH):
     return json_data
     
     
+    
 def get_prompt(question):
     
     text = f"""{question}
     """
     
-    PROMPT = f"""You are provided a DataFrame ```calendar_data(ID, status, summary, start, end , duration, attendees)```. This dataframe includes all you calenders.
-    Your task is generate python function to query this dataframe and answer the question. Output a python function object without any prefix instructionn and explaination.
+    PROMPT = f"""You are provided a Pandas dataframe named calendar_data, columns = [ID, status, summary, start, end , duration, attendees]. DataFrame calendar_data includes all you calenders. 
+    Your task is generate python function to query this dataframe and answer the question. Output a python code block without any prefix instructionn and explaination. 
+    Today's date is '2024-03-12 13:02:30-03:20'.
 
     The input have following columns:
     - ID: meeting ID;
@@ -41,20 +38,22 @@ def get_prompt(question):
     - start: the start date of meeting, date format: YYYY-MM-DD hh:mm:ss.fff-zz:xx. for example "2024-02-05 12:00:00-00:00";
     - end: the start date of meeting, date format: YYYY-MM-DD hh:mm:ss.fff-zz:xx, for example "2024-02-05 13:00:00-00:00";
     - duration: meeting duration (second);
-    - attendees: people who attend the meeting delimited by by the line terminator.
+    - attendees: people who attend the meeting delimited by the line terminator within 1 sentence.
 
-    Output using the folowing format:
-    code language: python
-    function name: query():
-    input: calendar_data
-    output is a executable python function without any prefix instructionn and explaination. If any python packages are required, add it.
+    Output is executable Python code by enclosing it in triple backticks:
+    ```python
+    <your code here>
+    ```
 
-    Today's date is '2024-03-12 13:02:30-03:20'.
+    The input of python code is a Pandas dataframe named calendar_data, and the answer is saved in variable answer.
 
-    For example, the output only have the following format:
+    For example, the output have the following format:
+    ```
     import pandas as pd 
     def query(calendar_data):
         return calendar_data[0]
+    answer = query(calendar_data)
+    ```
 
     Question to be resolved: {text} 
     """
@@ -65,6 +64,7 @@ def get_prompt(question):
 
 
 def get_code_completion(MODEL, PROMPT):
+    
     client = OpenAI(
         # This is the default and can be omitted
         api_key=os.environ.get("OPENAI_API_KEY"),
@@ -83,38 +83,7 @@ def get_code_completion(MODEL, PROMPT):
     # verify the output
     return chat_completion.choices[0].message.content
 
-# generated_code = get_completion(MODEL, PROMPT)
-# print(generated_code)
 
-def generate_code(CODE_PATH = "/generated_code", MODEL="gpt-4"):
-    if MODEL == "gpt-4":
-        FILE_NAME = 'GPT_4_generated_code.py'
-    else: # MODEL = "gpt-3.5-turbo"
-        FILE_NAME = 'gpt_3.5_turbo_generated_code.py'
-
-    fp = open(FILE_NAME, 'w')
-    # Add package dependency, eg import panda as pd 
-    fp.write(generated_code)
-    fp.close()
-
-
-
-
-# Import your module
-
-
-# Must update the function 
-importlib.reload(GPT_4_generated_code)
-
-count_correct, count_incorrect, count_code_error = 0, 0, 0
-
-try:
-    answer = GPT_4_generated_code.query(calendar_data)
-except:
-    # count how many unexecutable code OR incorrect answer we get.
-    # count_code_error += 1
-    pass
-print(answer)
 
 
 
@@ -127,11 +96,21 @@ def main():
     # MODEL = "gpt-3.5-turbo"
     
     
-    
-    
     calendar_Data = get_calendar_data(DATA_PATH=DATA_PATH)
+    queston_answer_pair = get_question(DATA_PATH=DATA_PATH)
+    
+    
+    for i in range():
+        get_prompt(queston_answer_pair[i]['question'])
+        
 
+    llm_reply_with_code = get_code_completion(MODEL, PROMPT)
 
+    _python_code_re_pattern = "```python\n(.*?)```"
+    llm_reply_without_code = re.sub(
+                _python_code_re_pattern, "", llm_reply_with_code, flags=re.DOTALL
+                )
+    python_code_list = re.findall(_python_code_re_pattern, llm_reply_with_code, re.DOTALL)
 
 
 
--- src/generated_code/GPT_4_generated_code.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def query(calendar_data):
-    today_start = pd.to_datetime("2024-03-12 00:00:00-00:00")
-    today_end = pd.to_datetime("2024-03-12 23:59:59-00:00")
-    calendar_data["start"] = pd.to_datetime(calendar_data["start"])
-    calendar_data["end"] = pd.to_datetime(calendar_data["end"])
-    return len(calendar_data.loc[(calendar_data["start"] >= today_start) & (calendar_data["end"] <= today_end)])
