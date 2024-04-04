b62ff21acb5fbaa518cd16fc191675334fdfaba6
Tianyu_Liu <tianyu.liu@ninjatech.ai>
2024-04-04 16:47:12 +1100

    Comparison - finish

--- src/old code/baseline.py
+++ src/old code/baseline.py
@@ -28,7 +28,7 @@ def get_prompt(question, examples):
     """
 
     PROMPT = f"""Answer the following question based on the JSON file with agenda from Google Calendar.\n Question: {task}'
-                \n\nJSON file with agenda:\n{examples}"""
+                \n\nJSON file with agenda:\n{examples}. Today's date is '2024-04-02 09:02:30' with date format '%Y-%m-%d %H:%M:%S'"""
                         
     return PROMPT
 
@@ -63,7 +63,6 @@ def main():
     
     DATA_PATH = "./data/"
     MODEL = "gpt-4"
-    # MODEL = "gpt-3.5-turbo"
     # MODEL = "ft:gpt-3.5-turbo-1106:ninjatech-ai-dev::8sQyzUKb"
     print(MODEL)
     
@@ -81,35 +80,40 @@ def main():
     calendar_data_JSON = calendar_data.to_json() 
     
     ground_true = []
-    
-    for q_a_pair in queston_answer_pairs:
+    for i in range(5):
+        
+        result = {'success': 0, 'wrong_answer': 0, 'error': 0}
+        for q_a_pair in queston_answer_pairs:
 
-        # print(f"""Question: {q_a_pair['question']}\n True answer: {q_a_pair['answer']}""")
-        
-        PROMPT = get_prompt(q_a_pair['question'], calendar_data_JSON)
-        
-        # question = q_a_pair['question']
-        # ground_true = (q_a_pair['answer'])
-        
-        llm_reply = get_completion(MODEL, PROMPT)
-        # print(f"""The answer of LLM: {llm_reply}""")
-        
-        # result analysis
-        question_answer_summary['question'] = q_a_pair['question']
-        question_answer_summary['true_answer'] = q_a_pair['answer']
-        question_answer_summary['generated_code'] = llm_reply
+            # print(f"""Question: {q_a_pair['question']}\n True answer: {q_a_pair['answer']}""")
+            
+            PROMPT = get_prompt(q_a_pair['question'], calendar_data_JSON)
+            
+            # question = q_a_pair['question']
+            # ground_true = (q_a_pair['answer'])
+            
+            llm_reply = get_completion(MODEL, PROMPT)
+            # print(f"""The answer of LLM: {llm_reply}""")
+            
+            if str(q_a_pair['answer']) in llm_reply.lower():
+                result['success'] += 1
+            else:
+                result['wrong_answer'] += 1
+            
+            # result analysis
+            question_answer_summary['question'] = q_a_pair['question']
+            question_answer_summary['true_answer'] = q_a_pair['answer']
+            question_answer_summary['generated_code'] = llm_reply
+        print(result)
+        result_list.append(result.copy())   
+    print(result_list)
     
-        result_list.append(question_answer_summary.copy())
-    
-    with open('baseline_' + f"""{MODEL}""" + '_results.json', 'w', encoding='utf-8') as f:
-        json.dump(result_list, f, indent=4)    
-    
+    # with open('baseline_' + f"""{MODEL}""" + '_results.json', 'w', encoding='utf-8') as f:
+    #     json.dump(result_list, f, indent=4)    
 
     print('End')
 
 
-        
-        
 
 
 if __name__ == '__main__':
